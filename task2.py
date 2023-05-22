d3d4f1efb5bfad928af8cdd6eea1e6c2bf344a7e
Oleksandr <merlin.ua.1@gmail.com>
2023-05-22 05:01:45 +0300

    Update task2

--- task2.py
+++ task2.py
@@ -1,22 +1,26 @@
+import os
 import shutil
-
 from imageai.Detection import VideoObjectDetection
 from moviepy.video.io.VideoFileClip import VideoFileClip
 from pytube import YouTube
 
-import os
+print('Lab5. Task2')
 
 execution_path = os.getcwd()
 
 if not os.path.exists('los-angeles-car-driving-cropped.mp4'):
     if not os.path.exists('los-angeles-car-driving.mp4'):
+        print('Downloading video from YouTube...')
         # Завантаження відео з YouTube
         yt = YouTube('https://www.youtube.com/watch?v=Cw0d-nqSNE8', use_oauth=True, allow_oauth_cache=True)
         stream = yt.streams.filter(file_extension='mp4').first()
         stream.download(output_path='./', filename='los-angeles-car-driving.mp4')
+        print('Download finished.')
 
+    print('Making subclip from video...')
     video = VideoFileClip('los-angeles-car-driving.mp4').subclip(50, 60)
     video.write_videofile('los-angeles-car-driving-cropped.mp4', fps=20)
+    print('Subclip created.')
 
 detector = VideoObjectDetection()
 detector.setModelTypeAsYOLOv3()
@@ -27,8 +31,9 @@ output_path = os.path.join(execution_path, 'output')
 shutil.rmtree(output_path, ignore_errors=True)
 os.makedirs(output_path)
 
+print('Starting video processing...')
 video_path = detector.detectObjectsFromVideo(
     input_file_path=os.path.join(execution_path, 'los-angeles-car-driving-cropped.mp4'),
     output_file_path=os.path.join(output_path, 'task2-output')
     , frames_per_second=20, log_progress=True)
-print(video_path)
+print('Video processed. Result path: {}.'.format(video_path))
